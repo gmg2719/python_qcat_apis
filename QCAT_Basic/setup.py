24d35022c802ff9f79541ecc6878c54fcfae4187
weihuachen <weihua.chen@gmail.com>
2018-08-10 16:06:01 -0500

    initial upload v0.0.3

---
+++ QCAT_Basic/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="QCAT_Basic",
+    version="0.0.3",
+    author="Weihua Chen",
+    author_email="weihua.chen@gmail.com",
+    description="A small package contains most useful QCAT interfaces",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/weihuachen/python_qcat_apis",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ),
+)
